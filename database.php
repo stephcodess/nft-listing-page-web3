ba414931766bc4c255685f7f91480efc03c09754
stephcodess <rajiolaoluwa0@gmail.com>
2022-05-30 19:57:04 +0100

    added database

--- database.php
+++ database.php
@@ -1,8 +1,12 @@
 <?php
-
-    $dbhost = 'localhost'; // Unlikely to require changing
-    $dbname = 'fetchnfts'; // Modify these...
-    $dbuser = 'root'; // ...variables according
-    $dbpass = ''; // ...to your installation
-    $conn=new mysqli($dbhost, $dbuser,$dbpass, $dbname) or die('unable to connect: ');
-?> 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
