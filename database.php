8401152e2d8fc890b9fd4a70fdd340f033f2a89f
stephcodess <rajiolaoluwa0@gmail.com>
2022-05-31 23:21:48 +0100

    changes

--- database.php
+++ database.php
@@ -1,20 +1,13 @@
 <?php
 //Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
-<?php
-
-$dbhost = 'localhost'; // Unlikely to require changing
-$dbname = 'fetchnfts'; // Modify these...
-$dbuser = 'root'; // ...variables according
-$dbpass = ''; // ...to your installation
-$conn=new mysqli($dbhost, $dbuser,$dbpass, $dbname) or die('unable to connect: ');
-?>
+<!-- -->
